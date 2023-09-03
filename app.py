056f3b8cfb17491cf3022bed11ed014967fe7d04
joshuainfluence <joshuajulius2030@gmail.com>
2023-09-03 10:24:38 +0100

    successful

---
+++ app.py
@@ -0,0 +1,9 @@
+# madlibs game
+print("Let's play a game")
+color = input("Enter a color: ")
+obj = input("Enter an object: ")
+celeb = input("Enter your favorite celebrity: ")
+
+print("Roses are " + color)
+print(obj + " are blue")
+print("I love " + celeb)
